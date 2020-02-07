d0ca51e5ceba109d5244cc92b28c0db48cdd5839
AlfieriFabio4C <56158271+AlfieriFabio4C@users.noreply.github.com>
2020-02-07 12:53:43 +0100

    Add files via upload
    Fino a Step 1

---
+++ SpaceInvaders.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author alfieri.fabio
+ */
+public class SpaceInvaders extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public SpaceInvaders() {
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+
+}
